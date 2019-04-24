857899a329fc4cee756e6994b4cdf9ebdf957256
conection.klon@gmail.com <conection.klon@gmail.com>
2019-04-24 17:34:05 -0500

    speech

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,9 @@
 
 
 angular.module('com.speechat', ['ngMaterial', 'ngMessages'])
-  .service('speechSvc', function () {
+  .service('speechSvc', function() {
 
-    this.speech = function (message) {
+    this.speech = function(message) {
       var speechMessage = new SpeechSynthesisUtterance();
 
       speechMessage.lang = 'es-Es'
@@ -15,31 +15,45 @@ angular.module('com.speechat', ['ngMaterial', 'ngMessages'])
 
       window.speechSynthesis.speak(speechMessage);
     }
-  }).controller('speechCtrl', ['$scope', 'speechSvc', function ($scope, speechSvc) {
+  }).controller('speechCtrl', ['$scope', 'speechSvc', function($scope, speechSvc) {
 
     $scope.accessToken = undefined;
-    $scope.speech = function () {
+    $scope.speech = function() {
       console.log('hola')
       speechSvc.speech('hola mundo');
     }
 
-    $scope.checkFacebook = function(){
+    $scope.checkFacebook = function() {
       FB.getLoginStatus(function(response) {
         console.log(response)
-        $scope.accessToken  = response.authResponse.accessToken;
-     });
+        $scope.accessToken = response.authResponse.accessToken;
+      });
     }
 
-    $scope.initFacebook = function () {
+    $scope.checkEvents = function() {
+      $scope.videoId = '680520909052858'
+      if ($scope.videoId) {
+        var source = new EventSource("https://streaming-graph.facebook.com/" + $scope.videoId + "/live_comments?access_token=" + $scope.accessToken);
+        source.onmessage = function(event) {
+          console.log(event)
+          var message = JSON.parse(event.data);
+          speechSvc.speech(message.message);
+          // Do something with event.message for example
+        };
+      }
+    }
+
+    $scope.initFacebook = function() {
       FB.init({
         appId: '2259098627641805',
         status: true,
         cookie: true,
         xfbml: true,
-        
-        version    : 'v3.2'
+
+        version: 'v3.2'
       });
-      FB.AppEvents.logPageView();   
+      FB.AppEvents.logPageView();
       $scope.checkFacebook();
+    
     };
   }]);
