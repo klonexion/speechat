753c56764a7b51b95e940a42466a88e386497a31
conection.klon@gmail.com <conection.klon@gmail.com>
2019-04-24 18:39:50 -0500

    twitch basic

--- scripts/main.js
+++ scripts/main.js
@@ -54,6 +54,35 @@ angular.module('com.speechat', ['ngMaterial', 'ngMessages'])
       });
       FB.AppEvents.logPageView();
       $scope.checkFacebook();
-    
+
     };
+
+    $scope.startTwitch = function() {
+      // Provide your token, username and channel. You can generate a token
+      // here: https://twitchapps.com/tmi/
+      const token = 'oauth:d1yase7q8eyo4qapjczik7en9340zv';
+      const username = 'kloneetv';
+      const channel = 'kloneetv';
+      // Instantiate clients.
+      const { api, chat, chatConstants } = new TwitchJs({ token, username });
+      // Get featured streams.
+      api.get('streams/featured').then(response => {
+        console.log(response);
+        // Do stuff ...
+      });
+      // Listen to all events.
+      const log = msg => console.log(msg);
+      chat.on(chatConstants.EVENTS.ALL, function(hay){
+        console.log(hay)
+        if(hay.command==="PRIVMSG"){
+          speechSvc.speech(hay.message)
+        }
+      });
+      // Connect ...
+      chat.connect().then(() => {
+        // ... and then join the channel.
+        chat.join(channel);
+      });
+    }
   }]);
+  //oauth:d1yase7q8eyo4qapjczik7en9340zv
